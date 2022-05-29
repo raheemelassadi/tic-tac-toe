81784c05f98a04f41045969d19ee4b2e7e7deb20
raheemelassadi <raheemelassadi1@gmail.com>
2022-05-28 23:55:51 -0700

    added logic for adding x and o

--- script.js
+++ script.js
@@ -1,22 +1,88 @@
-//** if there is one of something use methods..if there are mulitple use factory functions **
+//** if there is one of something use methods..if there are mulitple, use factory functions **
 
 // create gamebaord array using method
 const gameBoard = (function(){
-    const arr = [];
 
     //create player factory function
-    const createPlayer = (name, selection) => {
-        return {name, selection}
+    const createPlayer = (name, mark) => {
+        return {name, mark}
     }
 
-    //create players variables
+    //create player variables
     const playerOne = createPlayer('player1', 'x')
     const playerTwo = createPlayer('player2', 'o')
+
+    //winning combos
+    const winningCombos = [
+        [0,1,2],
+        [0,3,6],
+        [3,4,5],
+        [6,7,8],
+        [1,4,7],
+        [2,4,6],
+        [2,5,8],
+        [0,4,8]
+    ]
+
+    const playerOneArr = [];
+    const playerTwoArr = [];
+
+    //gameflow
+    const gameFlow = (function(){
+        //dom select td elements (the sqaures)
+        const squares = document.querySelectorAll('td')
+
+        //selectcurrplayer
+        let gameOver = false;
+        
+
+        let currPlayer = playerOne;
+
+        const playerNumber = document.querySelector('#player-number');
+        const playerMark = document.querySelector('#player-mark');
+
+        function checkWin(){
+            for (let i = 0; i < winningCombos.length; i++){
+                console.log(winningCombos[i])
+                if (winningCombos[i].every(elem => playerOneArr.includes(elem))){
+
+                }
+            }
+        }
+
+
+
+        //when sqaures are clicked, add the currplayers mark
+        for (let i = 0; i < squares.length; i++){
+            squares[i].addEventListener('click', function(){
+                const newElem = document.createElement('p')
+                //player1 logic
+                if (currPlayer == playerOne){
+                    playerOneArr.push(squares[i].id)
+                    playerOneArr.sort((a,b)=>a-b)
+                    playerNumber.textContent = '2'
+                    playerMark.textContent = 'O'
+                    newElem.textContent = playerOne.mark
+                    currPlayer = playerTwo
+                //player2 logic    
+                } else {
+                    playerTwoArr.push(squares[i].id)
+                    playerTwoArr.sort((a,b)=>a-b)
+                    playerNumber.textContent = '1';
+                    playerMark.textContent = 'X'
+                    newElem.textContent = playerTwo.mark 
+                    currPlayer = playerOne
+                }
+
+                squares[i].appendChild(newElem)                   
+            })
+        } 
+    })()
 })()
 
-//gameflow
-const gameFlow= (function(){
 
-})()
+
+
+
 
 
