2b74ccd34e418d4c6f27d3b7f7aa8e95038503c1
raheemelassadi <raheemelassadi1@gmail.com>
2022-05-29 18:49:09 -0700

    finished game logic

--- script.js
+++ script.js
@@ -3,7 +3,10 @@
 // create gamebaord array using method
 const gameBoard = (function(){
 
-    //create player factory function
+    //gameflow
+    const gameFlow = (function(){
+
+            //create player factory function
     const createPlayer = (name, mark) => {
         return {name, mark}
     }
@@ -24,33 +27,20 @@ const gameBoard = (function(){
         [0,4,8]
     ]
 
-    const playerOneArr = [];
-    const playerTwoArr = [];
 
-    //gameflow
-    const gameFlow = (function(){
         //dom select td elements (the sqaures)
         const squares = document.querySelectorAll('td')
 
         //selectcurrplayer
         let gameOver = false;
         
+        const playerOneArr = [];
+        const playerTwoArr = [];
 
         let currPlayer = playerOne;
 
         const playerNumber = document.querySelector('#player-number');
         const playerMark = document.querySelector('#player-mark');
-
-        function checkWin(){
-            for (let i = 0; i < winningCombos.length; i++){
-                console.log(winningCombos[i])
-                if (winningCombos[i].every(elem => playerOneArr.includes(elem))){
-
-                }
-            }
-        }
-
-
 
         //when sqaures are clicked, add the currplayers mark
         for (let i = 0; i < squares.length; i++){
@@ -58,24 +48,38 @@ const gameBoard = (function(){
                 const newElem = document.createElement('p')
                 //player1 logic
                 if (currPlayer == playerOne){
-                    playerOneArr.push(squares[i].id)
+                    playerOneArr.push(Number(squares[i].id))
                     playerOneArr.sort((a,b)=>a-b)
+                    for(let i = 0; i < winningCombos.length; i++){
+                        for (let j = 0; j < playerOneArr.length; j++){
+                            if (JSON.stringify(playerOneArr.slice(j, j + 3)) == JSON.stringify(winningCombos[i])){
+                                alert('Player One Wins!!')
+                        }
+                    }
+                    }
                     playerNumber.textContent = '2'
                     playerMark.textContent = 'O'
                     newElem.textContent = playerOne.mark
                     currPlayer = playerTwo
                 //player2 logic    
                 } else {
-                    playerTwoArr.push(squares[i].id)
+                    playerTwoArr.push(Number(squares[i].id))
                     playerTwoArr.sort((a,b)=>a-b)
+                    for(let i = 0; i < winningCombos.length; i++){
+                        for (let j = 0; j < playerTwoArr.length; j++){
+                            if (JSON.stringify(playerTwoArr.slice(j, j + 3)) == JSON.stringify(winningCombos[i])){
+                                alert('Player Two Wins!!')
+                        }
+                    }
+                    }
                     playerNumber.textContent = '1';
                     playerMark.textContent = 'X'
                     newElem.textContent = playerTwo.mark 
                     currPlayer = playerOne
                 }
-
                 squares[i].appendChild(newElem)                   
-            })
+            }, {once: true})
+
         } 
     })()
 })()
